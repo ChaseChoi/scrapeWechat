c371df7b1bbdba0a22965543f627ed4a5fc63938
ChaseChoi <caiguihao@gmail.com>
2017-10-20 20:49:06 +0800

    handle script but url wrong

--- getWechat.py
+++ getWechat.py
@@ -3,39 +3,29 @@ from selenium import webdriver
 import time
 import bs4
 import requests, re
-url = 'http://weixin.sogou.com'
-base ='https://mp.weixin.qq.com'
-browser = webdriver.Safari()
-browser.maximize_window()
-browser.get(url)
 
-searchBox = browser.find_element_by_id('query')
-searchBox.clear()
-searchBox.send_keys('中国共青团')
-button = browser.find_element_by_xpath('//*[@id="searchForm"]/div/input[4]')
-button.click()
-time.sleep(2)
-account = browser.find_element_by_xpath('//*[@id="sogou_vr_11002301_box_0"]/div/div[1]/a')
-accountURL = account.get_attribute('href')
-print(accountURL)
-time.sleep(2)
+base ='https://mp.weixin.qq.com'
+name = "广东共青团"
+searchURL = "http://weixin.sogou.com/weixin?type=1&s_from=input&query={}&ie=utf8&_sug_=y&_sug_type_=&w=01019900&sut=1139&sst0=1508495544059&lkt=0%2C0%2C0".format(name)
+res = requests.get(searchURL)
+res.raise_for_status()
+pattern = re.compile(r':"/s\?[=*;&\d\w]+"')
+soup = bs4.BeautifulSoup(res.text, "lxml")
+# get the corresponding URL
+account = soup.select('a[uigs="account_name_0"]')
+accountURL = account[0]['href']
+
 res = requests.get(accountURL)
 res.raise_for_status()
-pattern = re.compile(r'"content_url":"/s\?[=*;&\d\w]+"')
-soup = bs4.BeautifulSoup(res.text, "lxml")
-
-# script = soup.find_all("script", text=pattern)
-scripts = soup.find_all("script")
+accountSoup = bs4.BeautifulSoup(res.text, "lxml")
+time.sleep(3)
+scripts = accountSoup.find_all("script")
 scriptText = scripts[-2].text
+print(scriptText)
 extractURL = pattern.search(scriptText)
+time.sleep(3)
 print(extractURL.group())
-
-
-
-
-
-
-
-
-
+sliced = extractURL.group()
+part = sliced[2:-1]
+print(part)
 

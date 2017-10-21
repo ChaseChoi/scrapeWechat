22186d3eef13be491632cf02d734b5b853f93d91
ChaseChoi <caiguihao@gmail.com>
2017-10-21 20:26:43 +0800

    PhantomJS handle JS

--- getWechat.py
+++ getWechat.py
@@ -1,31 +1,52 @@
 #! /usr/bin/env python3
 from selenium import webdriver
-import time
-import bs4
-import requests, re
+import time, bs4, requests
 
 base ='https://mp.weixin.qq.com'
-name = "广东共青团"
-searchURL = "http://weixin.sogou.com/weixin?type=1&s_from=input&query={}&ie=utf8&_sug_=y&_sug_type_=&w=01019900&sut=1139&sst0=1508495544059&lkt=0%2C0%2C0".format(name)
+name = '凤凰新闻'
+accountList = ['央视新闻', '新浪新闻','凤凰新闻']
+searchURL = "http://weixin.sogou.com/weixin?type=1&s_from=input&query={}".format(name)
+
 res = requests.get(searchURL)
 res.raise_for_status()
-pattern = re.compile(r':"/s\?[=*;&\d\w]+"')
 soup = bs4.BeautifulSoup(res.text, "lxml")
-# get the corresponding URL
+# get the 1st URL
 account = soup.select('a[uigs="account_name_0"]')
+time.sleep(1)
 accountURL = account[0]['href']
 
+# go to detail page 
 res = requests.get(accountURL)
 res.raise_for_status()
-accountSoup = bs4.BeautifulSoup(res.text, "lxml")
+
+
+print(accountURL)
+browser = webdriver.PhantomJS("/Users/chasechoi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+browser.get(accountURL)
+
+
 time.sleep(3)
-scripts = accountSoup.find_all("script")
-scriptText = scripts[-2].text
-print(scriptText)
-extractURL = pattern.search(scriptText)
-time.sleep(3)
-print(extractURL.group())
-sliced = extractURL.group()
-part = sliced[2:-1]
-print(part)
+# html = browser.execute_script("return document.documentElement.outerHTML")
+html = browser.page_source
+accountSoup = bs4.BeautifulSoup(html, "lxml")
+
+time.sleep(2)
+contents = accountSoup.find_all(hrefs=True)
+
+partitialLink = contents[0]['hrefs']
+firstLink = base + partitialLink
+print(firstLink)
+
+# visit the content
+res = requests.get(firstLink)
+res.raise_for_status()
+detailPage = bs4.BeautifulSoup(res.text, "lxml")
+print(detailPage.title.text)
+myfile = open('/Users/chasechoi/Downloads/newsContent.html', 'wb')
+myfile.write(res.content)
+myfile.close()
+
+
+
+
 

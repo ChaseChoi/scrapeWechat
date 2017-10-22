a06bcb1aa08acea9d69abae9c1e6b720917e68b0
ChaseChoi <caiguihao@gmail.com>
2017-10-22 16:33:10 +0800

    finish basic func

--- getWechat.py
+++ getWechat.py
@@ -1,51 +1,73 @@
 #! /usr/bin/env python3
 from selenium import webdriver
-import time, bs4, requests
+from datetime import datetime
+import bs4, requests
+import os, time, sys
+
+def getAccountURL(searchURL):
+	res = requests.get(searchURL)
+	res.raise_for_status()
+	soup = bs4.BeautifulSoup(res.text, "lxml")
+	# get the 1st URL
+	account = soup.select('a[uigs="account_name_0"]')
+	time.sleep(10)
+	return account[0]['href']
+
+def getArticleURL(accountURL):
+	# go to detail page 
+	browser = webdriver.PhantomJS("/Users/chasechoi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
+	browser.get(accountURL)
+
+	html = browser.page_source
+	accountSoup = bs4.BeautifulSoup(html, "lxml")
+	time.sleep(2)
+	contents = accountSoup.find_all(hrefs=True)
+	try:
+		partitialLink = contents[0]['hrefs']
+		firstLink = base + partitialLink
+	except IndexError:
+		firstLink = None 
+		print('CAPTCHA!')
+	return firstLink
+
+def folderCreation():
+	path = os.path.join(os.getcwd(), datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
+	try:
+		os.makedirs(path)
+	except OSError as e:
+		if e.errno != errno.EEXIST:
+			raise
+		print("folder not exist!")
+	return path
+def writeToFile(path, title):
+	myfile = open("{}/{}.html".format(path, title), 'wb')
+	myfile.write(res.content)
+	myfile.close()
 
 base ='https://mp.weixin.qq.com'
-name = '凤凰新闻'
-accountList = ['央视新闻', '新浪新闻','凤凰新闻']
-searchURL = "http://weixin.sogou.com/weixin?type=1&s_from=input&query={}".format(name)
+accountList = ['央视新闻', '新浪新闻','凤凰新闻','羊城晚报', '罗辑思维', '小sa神', '亚马逊Kindle']
+query = 'http://weixin.sogou.com/weixin?type=1&s_from=input&query='
 
-res = requests.get(searchURL)
-res.raise_for_status()
-soup = bs4.BeautifulSoup(res.text, "lxml")
-# get the 1st URL
-account = soup.select('a[uigs="account_name_0"]')
-time.sleep(1)
-accountURL = account[0]['href']
+path = folderCreation()
+for index, account in enumerate(accountList):
 
-# go to detail page 
-res = requests.get(accountURL)
-res.raise_for_status()
+	searchURL = query + account
+	accountURL = getAccountURL(searchURL)
+	articleURL = getArticleURL(accountURL)
+	if articleURL != None:
+		print("#{}({}/{}): {}".format(account, index+1, len(accountList), accountURL))
+		# visit the content
+		res = requests.get(articleURL)
+		res.raise_for_status()
+		detailPage = bs4.BeautifulSoup(res.text, "lxml")
+		title = detailPage.title.text
+		print("标题: {}\n链接: {}\n".format(title, articleURL))
+		writeToFile(path, title)
+	else:
+		sys.exit()
 
 
-print(accountURL)
-browser = webdriver.PhantomJS("/Users/chasechoi/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs")
-browser.get(accountURL)
-
-
-time.sleep(3)
-# html = browser.execute_script("return document.documentElement.outerHTML")
-html = browser.page_source
-accountSoup = bs4.BeautifulSoup(html, "lxml")
-
-time.sleep(2)
-contents = accountSoup.find_all(hrefs=True)
-
-partitialLink = contents[0]['hrefs']
-firstLink = base + partitialLink
-print(firstLink)
-
-# visit the content
-res = requests.get(firstLink)
-res.raise_for_status()
-detailPage = bs4.BeautifulSoup(res.text, "lxml")
-print(detailPage.title.text)
-myfile = open('/Users/chasechoi/Downloads/newsContent.html', 'wb')
-myfile.write(res.content)
-myfile.close()
-
+print('{} files successfully written to {}'.format(len(accountList), path))
 
 
 
